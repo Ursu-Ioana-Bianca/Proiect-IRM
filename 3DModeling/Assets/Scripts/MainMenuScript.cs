1a631abacfa2666a2590ab6e5f38b38694c6077e
PricopCosminFII <79044611+PricopCosminFII@users.noreply.github.com>
2021-11-26 20:43:51 +0200

    vuforia

--- 3DModeling/Assets/Scripts/MainMenuScript.cs
+++ 3DModeling/Assets/Scripts/MainMenuScript.cs
@@ -1,11 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class MainMenuScript : MonoBehaviour
-{
-    public void QuitGame() {
-        Application.Quit();
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d3cef5e711ecb2e1c53b7bd17a7010873bc41f11449cd4851f94bbf4dea9166b
+size 237
