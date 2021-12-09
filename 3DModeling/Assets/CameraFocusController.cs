6eac56b32ba7ccaca12f238ee47c6d89ac8b840c
Teo Dragoi <teodorfdragoi@gmail.com>
2021-12-09 21:07:26 +0200

    Added test branch with Room Modifications

--- 3DModeling/Assets/CameraFocusController.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3c941198c8094c09c7cef2d3f845b8c42e67884a62d9080f0d3167055326a207
-size 1408
--- 3DModeling/Assets/Scripts/MainMenuScript.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d3cef5e711ecb2e1c53b7bd17a7010873bc41f11449cd4851f94bbf4dea9166b
-size 237
